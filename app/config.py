8c89667cc6c2ab9579ae3a5f3b2e01b1ba580b8d
Harsh1864 <harshbharodiya12@gmail.com>
2024-02-02 17:19:07 +0530

    hhhhh

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ class Setting(BaseSettings):
     database_username : str = "postgres"
     secret_key : str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
     algorithm : str = "HS256"
-    ACCESS_TOKEN_EXPIRE_HOURS : int = 24
+    ACCESS_TOKEN_EXPIRE_MINUTES : int = 24
 
     
 
