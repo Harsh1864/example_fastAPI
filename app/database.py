f5f6f3a772d3f5dfc24a3ef01da2e31e650c2ae8
Harsh1864 <harshbharodiya12@gmail.com>
2024-02-03 12:20:27 +0530

    changes done in database

--- app/database.py
+++ app/database.py
@@ -10,6 +10,7 @@ from psycopg2.extras import RealDictCursor
 DATABASE_URL = os.getenv('DATABASE_URL')
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
 engine = create_engine(DATABASE_URL)
 # engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
